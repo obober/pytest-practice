13d03100b83cfb1efaaf527cba4fb9765b97cfbd
Oles Bober <205997766+obober@users.noreply.github.com>
2025-04-07 15:24:43 -0700

    finished basic testing of JSON Placeholder API

--- test_jsonplaceholderapi.py
+++ test_jsonplaceholderapi.py
@@ -5,8 +5,7 @@ import requests
 def base_url():
     return "https://jsonplaceholder.typicode.com"
 
-
-# GET Request Test
+# Getting a resource
 # Test fetching a specific post
 @pytest.mark.jsonplaceholder
 def test_getting_resource(base_url):
@@ -18,10 +17,10 @@ def test_getting_resource(base_url):
     assert "body" in response.json()
     assert "userId" in response.json()
 
-# GET Requests Test
+# Listing all resources
 # Test fetching all posts
 @pytest.mark.jsonplaceholder
-def test_getting_resource(base_url):
+def test_listing_all_resources(base_url):
     url = base_url + "/posts"
     response = requests.get(url)
     assert response.status_code == 200
@@ -36,25 +35,89 @@ def test_getting_resource(base_url):
     assert "body" in response.json()[-1]
     assert "userId" in response.json()[-1]
 
-# POST Request Test
-# Test creating a new resource
+# Creating a resource
+# Test creating a new post
 @pytest.mark.jsonplaceholder
-@pytest.mark.xfail # Expecting to fail because I set "userId" to an integer and the response returns it as a string
+@pytest.mark.xfail # Expecting to fail because documentation says output for userId is an integer but its really a string
 def test_creating_resource(base_url):
     url = base_url + "/posts"
-    data = {
+    body = {
         "title": 'foo',
         "body": 'bar',
         "userId": 1
     }
-    response = requests.post(url, data)
+    response = requests.post(url, body)
     assert response.status_code == 201
     assert response.json()["id"] == 101
     assert response.json()["title"] == 'foo'
     assert response.json()["body"] == 'bar'
     assert response.json()["userId"] == 1
 
-# GET Request Test (parameterized)
+# Updating a resource
+# Test updating a specific post
+@pytest.mark.jsonplaceholder
+@pytest.mark.xfail # Expecting to fail because documentation says output for userId is an integer but its really a string
+def test_updating_resource(base_url):
+    url = base_url + "/posts/1"
+    body = {
+        "id": 1,
+        "title" : 'foo',
+        "body": 'bar',
+        "userId": 1
+    }
+    response = requests.put(url, body)
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+    assert response.json()["title"] == 'foo'
+    assert response.json()["body"] == 'bar'
+    assert response.json()["userId"] == 1
+
+# Patching a resource
+# Test patching a specific post
+@pytest.mark.jsonplaceholder
+def test_patching_resource(base_url):
+    url = base_url + "/posts/1"
+    body = {
+        "title": 'foo'
+    }
+    response = requests.patch(url, body)
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+    assert response.json()["title"] == 'foo'
+    assert "body" in response.json()
+    assert response.json()["userId"] == 1
+
+# Deleting a resource
+# Test deleting a specific post
+@pytest.mark.jsonplaceholder
+def test_deleting_resource(base_url):
+    url = base_url + "/posts/1"
+    response = requests.delete(url)
+    assert response.status_code == 200
+
+# Filtering resources
+# Test basic filtering using query parameters
+@pytest.mark.jsonplaceholder
+@pytest.mark.parametrize("filter, value", [("userId", 1), ("id", 1)])
+def test_filtering_resources(base_url, filter, value):
+    url = base_url + "/posts?" + filter + "=" + str(value)
+    response = requests.get(url)
+    assert response.status_code == 200
+
+    # assert that the first and last elements in the returned json array of dictionaries have the expected parameter and value
+    assert response.json()[0][filter] == value
+    assert response.json()[-1][filter] == value
+
+# Listing nested resources
+# Test getting all levels of a nested route
+@pytest.mark.jsonplaceholder
+@pytest.mark.parametrize("route, nest", [("posts", "comments"), ("albums", "photos"), ("users", "albums"), ("users", "todos"), ("users", "posts")])
+def test_listing_nested_resources(base_url, route, nest):
+    url = base_url + "/" + route + "/1/" + nest
+    response = requests.get(url)
+    assert response.status_code == 200
+
+# Listing multiple resources (parameterized)
 # Test fetching multiple posts
 @pytest.mark.jsonplaceholder
 @pytest.mark.parametrize("post_id, expected_title", [(1, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"), (2, "qui est esse")])
@@ -63,11 +126,3 @@ def test_getting_resources(base_url, post_id, expected_title):
     response = requests.get(url)
     assert response.status_code == 200
     assert response.json()["title"] == expected_title
-
-# DELETE Resource Test
-# Test deleting a specific post
-@pytest.mark.jsonplaceholder
-def test_deleting_resource(base_url):
-    url = base_url + "/posts/1"
-    response = requests.delete(url)
-    assert response.status_code == 200
